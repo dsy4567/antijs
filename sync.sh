c3a4ec30e3470f4e07dfd317939f8eceefc613f6
dsy4567 <dsy4567@outlook.com>
2022-05-27 12:37:00 +0800

    .

---
+++ sync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl https://www.4399.com/antijs/Antiindulgence.js -o Antiindulgence.js
+curl http://n.7k7kimg.cn/uploads/cdn/api/loginPlus/css/logFn.min.css -o logFn.min.css
+curl http://www.7k7kjs.cn/static/qike/play/js/play-fcm/play35.js -o play35.js
+curl http://www.7k7kjs.cn/static/qike/play/js/play-fcm/login-v3.js -o login-v3.js
+curl https://pic.my4399.com/re/cms/gamebox/js/fcmv2.js -o fcmv2.js
+curl http://h5.7k7k.com/js/anti/Anti.js
